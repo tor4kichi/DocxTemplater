2b84f4e844f69a41f11bd7a2c37382aaaefc9d15
tor4kichi <tor4kichi@gmail.com>
2026-04-19 21:58:18 +0900

    fix compile error for .netstandard2.0

---
+++ DocxTemplater/IsExternalInit.cs
@@ -0,0 +1,8 @@
+﻿
+#pragma warning disable IDE0130 // Namespace がフォルダー構造と一致しません
+namespace System.Runtime.CompilerServices
+#pragma warning restore IDE0130 // Namespace がフォルダー構造と一致しません
+{
+    internal class IsExternalInit { }
+}
+
