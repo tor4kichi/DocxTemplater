acf775dbee5ee4cd4c46dd5243454cc25f08ada6
tor4kichi <tor4kichi@gmail.com>
2026-04-24 16:18:39 +0900

    Deletes empty paragraphs that only retain line breaks that would have remained after template deletion. Includes table cells.

--- DocxTemplater/TemplateProcessor.cs
+++ DocxTemplater/TemplateProcessor.cs
@@ -154,12 +154,13 @@ namespace DocxTemplater
                     markedText.RemoveAttribute("mrk", null);                    
                 }
             }
-
-            // Remove blank paragraphs that only contain line breaks.
-            for (int i = element.FirstChild.ChildElements.Count - 1; i >= 0; i--)
-            {
-                if (element.FirstChild.ChildElements[i] is not Paragraph para) { continue; }
-
+            
+            // Deletes empty paragraphs that only retain line breaks that
+            // would have remained after template deletion.
+            // Includes table cells.
+            // Excludes intentional line breaks.
+            foreach (var para in element.Descendants<Paragraph>())
+            {               
                 if (para.ChildElements.Count == 2
                     && para.ChildElements.ElementAtOrDefault(0) is ParagraphProperties
                     && para.ChildElements.ElementAtOrDefault(1) is Run run
